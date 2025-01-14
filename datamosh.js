d52fbbc80de67a27288cb09e0bcd1806b157a8a2
Cameron Alexander <emptyflash@gmail.com>
2025-01-14 16:44:57 -0500

    Fix resolution and handle hydra outputs

--- datamosh.js
+++ datamosh.js
@@ -1,80 +1,95 @@
-// Reverse Datamosh with WebCodecs.
-// Author: Amagi
-//
-// Unlike normal Datamosh, this applies the delta frame over and over, resulting adn over-animated frames.
+// Adapted from codepen by Amagi: https://codepen.io/fand/pen/Vwojwqm
 
-export default async function datamoshSource(source, dest, params) {
-  params = params || {
-    speed: 2,
-  };
-  let canvas = document.getElementById("datamoshCanvas");
-  if (!canvas) {
-    canvas = document.createElement("canvas");
-  }
-  await dest.init({ src: canvas })
-  const ctx = canvas.getContext("2d");
+async function datamosh(source, params) {
+	params = params || {
+		speed: 2,
+	};
+	const hydra = params.hydra || window.hydraSynth;
+	let output;
+	if (typeof source === 'function') {
+		if (!window.newHydra) {
+			const newHydra = new Hydra({
+				makeGlobal: false
+			});
+			window.newHydra = newHydra;
+		}
+		const datamoshSource = newHydra.createSource(newHydra.s.length);
+		source(newHydra.synth)
+		await datamoshSource.init({
+			src: newHydra.canvas
+		});
+		source = datamoshSource;
+	}
+	const newSource = hydraSynth.createSource(hydraSynth.s.length);
+	let canvas = document.getElementById("datamoshCanvas");
+	if (!canvas) {
+		canvas = document.createElement("canvas");
+	}
+	await newSource.init({
+		src: canvas
+	})
+	const ctx = canvas.getContext("2d");
 
-  const encoder = new VideoEncoder({
-    output: handleEncodedChunk,
-    error: (err) => console.error("Encoder error:", err),
-  });
+	const encoder = new VideoEncoder({
+		output: handleEncodedChunk,
+		error: (err) => console.error("Encoder error:", err),
+	});
 
-  const decoder = new VideoDecoder({
-    output: handleDecodedFrame,
-    error: (err) => console.error("Decoder error:", err),
-  });
+	const decoder = new VideoDecoder({
+		output: handleDecodedFrame,
+		error: (err) => console.error("Decoder error:", err),
+	});
 
-  decoder.configure({
-    codec: "vp8",
-  });
+	decoder.configure({
+		codec: "vp8",
+	});
 
-  // Start the processing loop
-  function processFrame() {
-    if (source.src) {
-      const width = source.src.videoWidth || source.src.width
-      const height = source.src.videoHeight || source.src.videoHeight
-      if (width > 0 && height > 0) {
-        if (encoder.state === 'unconfigured') {
-          encoder.configure({
-            codec: "vp8",
-            width,
-            height,
-            bitrate: 1_000_000, // 1Mbps bitrate
-          });
-        }
-        // Video is playing
-        const frame = new VideoFrame(source.src, {
-          timestamp: performance.now() * 1000,
-        });
-        encoder.encode(frame, { keyFrame: params.keyFrame });
-        params.keyFrame = false;
-        frame.close();
-      }
-    }
-    requestAnimationFrame(processFrame);
-  }
+	// Start the processing loop
+	function processFrame() {
+		if (source.src) {
+			const width = source.src.videoWidth || source.src.width
+			const height = source.src.videoHeight || source.src.height
+			if (width > 0 && height > 0) {
+				if (encoder.state === 'unconfigured') {
+					canvas.width = width;
+					canvas.height = height;
+					encoder.configure({
+						codec: "vp8",
+						width,
+						height,
+						bitrate: 1_000_000, // 1Mbps bitrate
+					});
+				}
+				// Video is playing
+				const frame = new VideoFrame(source.src, {
+					timestamp: performance.now() * 1000,
+				});
+				encoder.encode(frame, {
+					keyFrame: params.keyFrame
+				});
+				params.keyFrame = false;
+				frame.close();
+			}
+		}
+		requestAnimationFrame(processFrame);
+	}
 
-  function handleEncodedChunk(chunk) {
-    if (chunk.type === "key") {
-      decoder.decode(chunk);
-    } else {
-      for (let i = 0; i < params.speed; i++) {
-        decoder.decode(chunk);
-      }
-    }
-  }
+	function handleEncodedChunk(chunk) {
+		if (chunk.type === "key") {
+			decoder.decode(chunk);
+		} else {
+			for (let i = 0; i < params.speed; i++) {
+				decoder.decode(chunk);
+			}
+		}
+	}
 
-  function handleDecodedFrame(frame) {
-    if (source.src.width) {
-      canvas.width = source.src.width
-    }
-    if (source.src.height) {
-      canvas.height = source.src.height
-    }
-    // Draw the decoded frame onto the canvas
-    ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
-    frame.close();
-  }
+	function handleDecodedFrame(frame) {
+		// Draw the decoded frame onto the canvas
+		ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+		frame.close();
+	}
 
-  processFrame(); // Start encoding and decoding
+	processFrame(); // Start encoding and decoding
+	return newSource;
 }
